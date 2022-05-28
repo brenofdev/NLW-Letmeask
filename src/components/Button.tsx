f97982cf07aa68b24b7af79b6f4be25edaa81fbf
Breno <brenofcb1@gmail.com>
2022-05-27 21:37:15 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ButtonHTMLAttributes } from "react";
+
+import '../styles/button.css';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean
+};
+
+export function Button({ isOutlined = false, ...props}: ButtonProps) {
+  return (
+    <button 
+    className={`button ${isOutlined ? 'outlined' : '' }`} 
+    {...props} 
+    />
+  )
+}
+
---
+++ src/components/Question.tsx
@@ -0,0 +1,38 @@
+import React, { ReactNode } from 'react';
+import cx from 'classnames';
+
+import '../styles/question.css';
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+  children?: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
+}
+
+export function Question(props: QuestionProps) {
+  return (
+    <div 
+      className={cx(
+        'question', 
+        { answered: props.isAnswered },
+        { highlighted: props.isHighlighted && !props.isAnswered },
+      )}
+    >
+      <p>{props.content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={props.author.avatar} alt={props.author.name} />
+          <span>{props.author.name}</span>
+        </div>
+        <div>
+          {props.children}
+        </div>
+      </footer>
+    </div>
+  );
+}
